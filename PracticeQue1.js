56672e7af99b1f03e140de240c609ee53edb22da
Princz.1 <150322497+princz-1@users.noreply.github.com>
2023-11-18 15:01:12 +0530

    Practice Question.1
    // Create a const object called "product"

---
+++ PracticeQue1.js
@@ -0,0 +1,11 @@
+// Create a const object called "product"
+const product ={
+  Name : "Black pen",
+  rating : 4,
+  offer : -25,
+  deal : true,
+  price : 120.0,
+  
+  
+};
+console.log(product);
