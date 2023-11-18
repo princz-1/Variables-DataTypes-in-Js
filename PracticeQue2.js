d24cf9ee15885de1644e0283af727003bf7f0405
Princz.1 <150322497+princz-1@users.noreply.github.com>
2023-11-18 15:06:33 +0530

    Practice Question.2
    // Create a const object called "social media profile"

---
+++ PracticeQue2.js
@@ -0,0 +1,10 @@
+// Create a const object called "social media profile"
+const profile ={
+  username : "Princz.1",
+  following : 123,
+  followers : 14000,
+  posts : 12,
+  bio : "web Developer",
+  isFollow : true,
+};
+console.log(profile);
